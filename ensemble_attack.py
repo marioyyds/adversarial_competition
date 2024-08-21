2ee3c560e58ffa8b353d4db75919f7f444c5d312
mario <marioDu@163.com>
2024-08-21 12:35:11 +0800

    feat: ensemble more models and get a score 59.9.

--- ensemble_attack.py
+++ ensemble_attack.py
@@ -104,8 +104,8 @@ if __name__ == '__main__':
 
     print("Model to generate adv examples")
     # delete one model if OOM (all 8 models need around 12G)
-    model_name_list = ['resnet50.a1_in1k', 'inception_v3', 'efficientnet_b0.ra4_e3600_r224_in1k', 'resnet18', 'resnet34','wide_resnet50_2.racm_in1k', 'vgg16.tv_in1k']
-    model_input_size = [224, 229, 224, 224, 224, 224, 224]
+    model_name_list = ['vit_tiny_patch16_224.augreg_in21k_ft_in1k', 'inception_v3', 'efficientnet_b0.ra4_e3600_r224_in1k', 'resnet18', 'resnet34','wide_resnet50_2.racm_in1k', 'vgg16.tv_in1k', 'resnet18.a1_in1k']
+    model_input_size = [224, 229, 224, 224, 224, 224, 224, 224]
     print(model_name_list)
     model_list = []
     for model_name in model_name_list:
@@ -123,8 +123,8 @@ if __name__ == '__main__':
 
     # eval model
     print("Model to predict adv examples's confidence")
-    model_name_list = ['mobilenet_v2', 'resnet101.a1h_in1k', 'vit_tiny_patch16_224.augreg_in21k_ft_in1k', 'efficientnet_lite0.ra_in1k']
-    model_input_size = [224, 224, 224, 224]
+    model_name_list = ['mobilenet_v2', 'resnet101.a1h_in1k', 'resnet50.a1_in1k']
+    model_input_size = [224, 224, 224]
     print(model_name_list)
     model_list = []
     for model_name in model_name_list:
